67d35bd40cfc4b12b525a143b564bcd857cb2de1
Mariusz <mnagrocki@gmail.com>
2020-02-15 20:19:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            Console.WriteLine(a%b);
+           
+        }
+    }
+}
